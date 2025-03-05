6c9cbd8ad2ef16bc1304cf7bd195ff5af4eff7fa
ilcors-dev <corsetti.luca28@gmail.com>
2025-03-05 22:04:53 +0100

    fix sprint2 scale architecture image

---
+++ project/sprint3/Arch_monitoringdevice_tests.py
@@ -0,0 +1,36 @@
+### conda install diagrams
+from diagrams import Cluster, Diagram, Edge
+from diagrams.custom import Custom
+import os
+os.environ['PATH'] += os.pathsep + 'C:/Program Files/Graphviz/bin/'
+
+graphattr = {     #https://www.graphviz.org/doc/info/attrs.html
+    'fontsize': '22',
+}
+
+nodeattr = {   
+    'fontsize': '22',
+    'bgcolor': 'lightyellow'
+}
+
+eventedgeattr = {
+    'color': 'red',
+    'style': 'dotted'
+}
+evattr = {
+    'color': 'darkgreen',
+    'style': 'dotted'
+}
+with Diagram('monitoringdevice_testsArch', show=False, outformat='png', graph_attr=graphattr) as diag:
+  with Cluster('env'):
+     sys = Custom('','./qakicons/system.png')
+### see https://renenyffenegger.ch/notes/tools/Graphviz/attributes/label/HTML-like/index
+     with Cluster('ctxmonitoringdevice_tests', graph_attr=nodeattr):
+          led=Custom('led','./qakicons/symActorSmall.png')
+          led_device=Custom('led_device','./qakicons/symActorSmall.png')
+          sonar=Custom('sonar','./qakicons/symActorSmall.png')
+          sonar_device=Custom('sonar_device','./qakicons/symActorSmall.png')
+     sonar_device >> Edge( label='sonar_data', **eventedgeattr, decorate='true', fontcolor='red') >> sonar
+     sonar_device >> Edge(color='blue', style='solid',  decorate='true', label='<sonar_sensitivity &nbsp; >',  fontcolor='blue') >> sonar
+     led >> Edge(color='blue', style='solid',  decorate='true', label='<update_physical_led_mode &nbsp; >',  fontcolor='blue') >> led_device
+diag
--- project/sprint3/Arch_wis24.py
+++ project/sprint3/Arch_wis24.py
@@ -25,6 +25,23 @@ with Diagram('wis24Arch', show=False, outformat='png', graph_attr=graphattr) as 
   with Cluster('env'):
      sys = Custom('','./qakicons/system.png')
 ### see https://renenyffenegger.ch/notes/tools/Graphviz/attributes/label/HTML-like/index
+     with Cluster('ctxbasicrobot', graph_attr=nodeattr):
+          basicrobot=Custom('basicrobot(ext)','./qakicons/externalQActor.png')
      with Cluster('ctxwis24', graph_attr=nodeattr):
           wis=Custom('wis','./qakicons/symActorSmall.png')
+          oprobot=Custom('oprobot','./qakicons/symActorSmall.png')
+          incinerator=Custom('incinerator','./qakicons/symActorSmall.png')
+     with Cluster('ctxmonitoringdevice', graph_attr=nodeattr):
+          sonar_device=Custom('sonar_device(ext)','./qakicons/externalQActor.png')
+          led=Custom('led(ext)','./qakicons/externalQActor.png')
+          sonar=Custom('sonar(ext)','./qakicons/externalQActor.png')
+     with Cluster('ctxscale', graph_attr=nodeattr):
+          scale_device=Custom('scale_device(ext)','./qakicons/externalQActor.png')
+     oprobot >> Edge(color='magenta', style='solid', decorate='true', label='<engage &nbsp; moverobot<font color="darkgreen"> moverobotdone moverobotfailed</font> &nbsp; >',  fontcolor='magenta') >> basicrobot
+     wis >> Edge(color='magenta', style='solid', decorate='true', label='<getrp<font color="darkgreen"> getrp_status</font> &nbsp; depositrp<font color="darkgreen"> depositrp_status</font> &nbsp; depositash<font color="darkgreen"> depositash_status</font> &nbsp; gohome<font color="darkgreen"> gohome_status</font> &nbsp; extractash<font color="darkgreen"> extractash_status</font> &nbsp; >',  fontcolor='magenta') >> oprobot
+     oprobot >> Edge(color='blue', style='solid',  decorate='true', label='<load_ash &nbsp; >',  fontcolor='blue') >> sonar_device
+     incinerator >> Edge(color='blue', style='solid',  decorate='true', label='<burning &nbsp; finishedBurning &nbsp; >',  fontcolor='blue') >> wis
+     oprobot >> Edge(color='blue', style='solid',  decorate='true', label='<unload_weight &nbsp; >',  fontcolor='blue') >> scale_device
+     wis >> Edge(color='blue', style='solid',  decorate='true', label='<update_led_mode &nbsp; >',  fontcolor='blue') >> led
+     wis >> Edge(color='blue', style='solid',  decorate='true', label='<startIncinerator &nbsp; startBurning &nbsp; >',  fontcolor='blue') >> incinerator
 diag
