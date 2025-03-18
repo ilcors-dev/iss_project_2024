d7fc9b0e9ba662ae6f992c0c64c3c0fded259fea
ilcors-dev <corsetti.luca28@gmail.com>
2025-03-18 09:29:09 +0100

    stuff

--- project/monitoringdevice_raspberry/monitoringdevice.pl
+++ project/monitoringdevice_raspberry/monitoringdevice.pl
@@ -7,6 +7,7 @@ event( sonar_data, distance(D) ).
 dispatch( sonar_sensitivity, sonar_sensitivity(S) ).
 dispatch( ash_measurement, ash_measurement(L) ).
 dispatch( load_ash, load_ash(D) ).
+dispatch( unload_ash, unload_ash(D) ).
 event( mqtt_info, mqtt_info(MSG) ).
 %====================================================================================
 context(ctxwis24, "192.168.0.200",  "TCP", "8121").
@@ -20,3 +21,5 @@ context(ctxmonitoringdevice, "localhost",  "TCP", "8122").
  static(sonar).
   qactor( sonar_device, ctxmonitoringdevice, "it.unibo.sonar_device.Sonar_device").
  static(sonar_device).
+  qactor( mock_ash_unloader_external, ctxmonitoringdevice, "it.unibo.mock_ash_unloader_external.Mock_ash_unloader_external").
+ static(mock_ash_unloader_external).
